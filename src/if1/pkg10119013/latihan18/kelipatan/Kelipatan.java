61d735b6e59ca3d175dbcf798b86c819e5e3fc4f
Fiona Avila <fionaavila15@gmail.com>
2020-10-28 13:30:41 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan18/kelipatan/Kelipatan.java
@@ -0,0 +1,32 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan18.kelipatan;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+
+public class Kelipatan {
+    
+   public static void main(String[] args) {
+        
+       double i,j;
+       
+        for(i=1;i<=10;i++){
+            j = 3.5 * i;
+            System.out.println(j);
+            if(j==35)
+                break;
+        }
+        
+    }
+    
+}
